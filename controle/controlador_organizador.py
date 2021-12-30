d1c0a841e3b28abd83f8a2116094892d3af1e123
Matheus <math.a.monteiro@gmail.com>
2021-12-30 09:21:45 -0300

    function abrir_menu now holds generic menu logic

--- controle/controlador_organizador.py
+++ controle/controlador_organizador.py
@@ -13,14 +13,18 @@ class ControladorOrganizador(Controlador):
     def abrir_menu_inicial(self):
 
         opcoes = {1: self.cadastrar}
+        opcoes_validas = [0, 1]
+        menu = self.tela.mostrar_menu_inicial
+
+        self.abrir_menu(menu, opcoes, opcoes_validas)
+
+    #move function to parent class
+    def abrir_menu(self, menu = None, opcoes = {}, opcoes_validas = []):
 
         while True:
-            self.tela.mostrar_menu_inicial()
-            opcoes_validas = [0, 1]
+            menu()
             opcao_escolhida = self.tela.ler_inteiro(opcoes_validas)
             funcao_escolhida = opcoes[opcao_escolhida]
             funcao_escolhida()
-
-
         
 
