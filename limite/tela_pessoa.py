b9945bc2bdc358169db697f91904515e7f64365c
Matheus Antunes Monteiro <matheus@debian>
2022-03-07 10:34:41 -0300

    improvements to organizador.cadastro validation

--- limite/tela_pessoa.py
+++ limite/tela_pessoa.py
@@ -10,22 +10,31 @@ class TelaPessoa(TelaIntegrante):
 
         super().__init__(controlador)
 
-    def init_tela_cadastro(self):
+    def init_tela_cadastro(self, values):
 
         sg.ChangeLookAndFeel('Reddit')
 
         layout = [
-            [sg.Text("Nome:", size=(15, 1)), sg.InputText(key="nome")],
-            [sg.Text("CPF:", size=(15, 1)), sg.InputText(key="cpf")],
-            [sg.Text("Nascimento:", size=(15, 1)), sg.InputText(
-                key="nascimento"), sg.CalendarButton("Nascimento", target="nascimento", format="%d/%m/%Y")],
-            [sg.Text("CEP:", size=(15, 1)), sg.InputText(key="cep")],
-            [sg.Text("Rua:", size=(15, 1)), sg.InputText(key="rua")],
-            [sg.Text("Número:", size=(15, 1)), sg.InputText(key="numero")],
-            [sg.Text("Bairro:", size=(15, 1)), sg.InputText(key="bairro")],
-            [sg.Text("Cidade:", size=(15, 1)), sg.InputText(key="cidade")],
-            [sg.Text("Estado:", size=(15, 1)), sg.InputText(key="estado")],
-            [sg.Submit("Enviar"), sg.Cancel("Cancelar")]
+            [sg.Text("Nome:", size=(15, 1)), sg.InputText(
+                values["nome"] if values else None, key="nome")],
+            [sg.Text("CPF:", size=(15, 1)), sg.InputText(
+                values["cpf"] if values else None, key="cpf")],
+            [sg.Text("Nascimento:", size=(15, 1)), sg.InputText(values["nascimento"].strftime("%d/%m/%Y") if values else None,
+                                                                key="nascimento"), sg.CalendarButton("Calendário", target="nascimento", format="%d/%m/%Y")],
+            [sg.Text("CEP:", size=(15, 1)), sg.InputText(
+                values["cep"] if values else None, key="cep")],
+            [sg.Text("Rua:", size=(15, 1)), sg.InputText(
+                values["rua"] if values else None, key="rua")],
+            [sg.Text("Número:", size=(15, 1)), sg.InputText(
+                values["numero"] if values else None, key="numero")],
+            [sg.Text("Bairro:", size=(15, 1)), sg.InputText(
+                values["bairro"] if values else None, key="bairro")],
+            [sg.Text("Cidade:", size=(15, 1)), sg.InputText(
+                values["cidade"] if values else None, key="cidade")],
+            [sg.Text("Estado:", size=(15, 1)), sg.InputText(
+                values["estado"] if values else None, key="estado")],
+            [sg.Submit("Enviar", key="enviar"),
+             sg.Cancel("Cancelar", key=0)]
         ]
 
         self.window = sg.Window(
@@ -37,17 +46,26 @@ class TelaPessoa(TelaIntegrante):
 
     def mostrar_tela_cadastro(self, alterar=False):
 
-        self.init_tela_cadastro()
-        button, values = self.open()
-        self.close()
+        values = None
 
-        values["numero"] = int(values["numero"])
-        values["nascimento"] = datetime.strptime(
-            values["nascimento"], "%d/%m/%Y")
+        while(True):
+            try:
+                self.init_tela_cadastro(values)
+                button, values = self.open()
+                self.close()
 
-        self.self_validar_cadastro(values)
+                if(button == 0):
+                    return
 
-        return values
+                values["numero"] = int(values["numero"])
+                values["nascimento"] = datetime.strptime(
+                    values["nascimento"], "%d/%m/%Y")
+
+                self.self_validar_cadastro(values)
+
+                return values
+            except ValueError as err:
+                self.show_message("Erro", err)
 
     def mostrar_tela_cadastro_repetido(self, pessoa):
         self.mostrar_mensagem(
